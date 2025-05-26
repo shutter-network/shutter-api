dc7a4828ca9facce80f05d0f5ead9b6e6873b264
blockchainluffy <jainpunit1997@gmail.com>
2025-05-26 17:47:58 +0530

    updated error message from external keypers and added block time based msg

--- internal/usecase/crypto.go
+++ internal/usecase/crypto.go
@@ -167,11 +167,19 @@ func (uc *CryptoUsecase) GetDecryptionKey(ctx context.Context, identity string) 
 	})
 	if err != nil {
 		if err == pgx.ErrNoRows {
+			if registrationData.Timestamp%5 != 0 {
+				err := httpError.NewHttpError(
+					fmt.Sprintf("decryption will happen based on gnosis block time, please try again later after %d seconds", registrationData.Timestamp%5),
+					"",
+					http.StatusAccepted,
+				)
+				return nil, &err
+			}
 			// no data found try querying from other keyper via http
 			decKey, err := uc.getDecryptionKeyFromExternalKeyper(ctx, int64(registrationData.Eon), identity)
 			if err != nil {
 				err := httpError.NewHttpError(
-					err.Error(),
+					fmt.Sprintf("error while querying decryption key from external keyper: %s", err.Error()),
 					"",
 					http.StatusInternalServerError,
 				)

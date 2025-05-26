c43ccbbbfc61aefa39cf062b9ecdbdca8c06949c
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-05-26 13:39:16 +0300

    Update crypto.go

--- internal/service/crypto.go
+++ internal/service/crypto.go
@@ -153,7 +153,7 @@ func (svc *CryptoService) RegisterIdentity(ctx *gin.Context) {
 //	@Description	Provides a way for clients to easily decrypt their encrypted message for which they have registered the identity for. Timestamp with which the identity was registered should have been passed for the message to be decrypted successfully.
 //	@Tags			Crypto
 //	@Produce		json
-//	@Param			identity			query		string		true	"Identity used for registeration and encrypting the message."
+//	@Param			identity			query		string		true	"Identity used for registration and encrypting the message."
 //	@Param			encryptedCommitment	query		string		true	"Encrypted commitment is the clients encrypted message."
 //	@Success		200					{object}	[]byte		"Success."
 //	@Failure		400					{object}	error.Http	"Invalid Decrypt commitment request."
